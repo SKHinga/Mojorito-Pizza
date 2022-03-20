3d3311192ee38de1f480f251d8176d4d6a4feea5
SKHinga <samuelhinga200@gmail.com>
2022-03-20 03:32:42 +0300

    Ft: add pizza image and note

--- JS/main.js
+++ JS/main.js
@@ -4,4 +4,4 @@ const navMenu = document.querySelector('.nav-item');
 hamburger.addEventListener('click', ()=>{
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
-});
+})
